13decccf4e904580a76fd73dfa8646b81c9815c9
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-01-26 22:23:22 +0100

    new benchmarks

---
+++ bench/era5_create_dataset_zarr2.py
@@ -0,0 +1,20 @@
+# /// script
+# dependencies = [
+#     "fsspec",
+#     "gcsfs@git+https://github.com/fsspec/gcsfs.git#egg=gcsfs",
+#     "zarr==2.18",
+#     "xarray==2025.01.1",
+# ]
+# ///
+import xarray
+import time
+
+uri = 'gs://gcp-public-data-arco-era5/ar/full_37-1h-0p25deg-chunk-1.zarr-v3'
+start = time.time()
+xarray.open_dataset(
+	uri,     
+	engine='zarr',     
+	consolidated=False,     
+	storage_options = {'token': 'anon'})
+
+print(f'elapsed:  {time.time() - start:0.2f} seconds')
